c9369e19066d0787c64fc842658a620efbb41d58
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2023-08-15 20:23:32 -0300

    Add files via upload

--- cadastro.c
+++ cadastro.c
@@ -7,30 +7,30 @@ main() {
     char matricula[10];
     char endereco[100];
     char curso[50];
-    char periodo[10];
+    int periodo;
     char disciplina[50];
     float mensalidade;
 
     printf("Digite o nome do aluno: ");
-    fgets(nome, 50, stdin);
+    fgets(nome, sizeof(nome), stdin);
     printf("Digite a idade do aluno: ");
     scanf("%d", &idade);
     printf("Digite a matricula do aluno: ");
-    scanf("%s", matricula);
+    fgets(matricula, sizeof(matricula), stdin);
     printf("Digite o endereco do aluno: ");
     fflush(stdin);
-    fgets(endereco, 100, stdin);
+    fgets(endereco, sizeof(endereco), stdin);
     printf("Digite o curso do aluno: ");
-    fgets(curso, 50, stdin);
+    fgets(curso, sizeof(curso), stdin);
     printf("Digite o periodo do aluno: ");
     scanf("%s", periodo);
     printf("Digite a disciplina do aluno: ");
     fflush(stdin);
-    fgets(disciplina, 50, stdin);
+    fgets(disciplina, sizeof(disciplina), stdin);
     printf("Digite o valor da mensalidade do aluno: ");
     scanf("%f", &mensalidade, "reais");
 
-    printf("\n\nNome: %s", nome);
+    printf("\nNome: %s", nome);
     printf("Idade: %d", idade);
     printf("Matricula: %s", matricula);
     printf("Endereco: %s", endereco);

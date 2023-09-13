3655efab4568c219f097e83eca16efd40ee1991b
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2023-09-12 21:02:32 -0300

    Add files via upload

---
+++ repeticao.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+main() {
+    /*Exercício 01*/
+    /*int num, cont = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    while (cont <= num) {
+        printf("%d\n", cont);
+        cont++;
+    }*/
+
+    /*Exercício 02*/
+    /*int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    do {
+        printf("%d\n", num);
+        num--;
+    } while (num >= 0);*/
+    
+    /*Exercício 03*/
+    /*int num, impares;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    impares = 1;
+
+    Método ineficiente
+    for (int i = 0; num >= impares ; i++) {
+        printf("\n%d", impares);
+        impares += 2;
+    }
+
+    
+    while (num >= impares) {
+        printf("\n%d", impares);
+        impares += 2;
+    }*/
+
+    /*Exercício 04*/
+    int num
+
+    return 0;
+}

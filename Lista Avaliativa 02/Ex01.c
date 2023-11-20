5e6380f22f4157f5f9efadbe534667955295232e
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2023-11-20 13:32:49 -0300

    Update Ex01.c

--- Lista Avaliativa 02/Ex01.c
+++ Lista Avaliativa 02/Ex01.c
@@ -1,5 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int validaQuantidade(int quantidade) {
-    
+int validaQuantidade();
+float calculaSalario(int quantidade);
+
+int main() {
+    int quantidade;
+    float salarioFinal;
+
+    quantidade = validaQuantidade();
+    salarioFinal = calculaSalario(quantidade);
+
+    printf("O salario final eh: %.2f\n", salarioFinal);
+
+    return 0;
+}
+
+int validaQuantidade() {
+    int quantidade;
+    float bonus = 0.0;
+
+    printf("Digite a quantidade produzida: ");
+    scanf("%d", &quantidade);
+
+    if (quantidade <= 50) {
+        bonus = 0;
+    } else if (quantidade > 50 && quantidade <= 80) {
+        bonus = 0.5;
+    } else {
+        bonus = 0.75;
+    }
+
+    return quantidade;
+}
+
+float calculaSalario(int quantidade) {
+    float salario = 600.00;
+    float bonus;
+
+    if (quantidade <= 50) {
+        bonus = 0;
+    } else if (quantidade > 50 && quantidade <= 80) {
+        bonus = 0.5;
+    } else {
+        bonus = 0.75;
+    }
+
+    float salarioFinal = salario + (salario * bonus);
+
+    return salarioFinal;
 }

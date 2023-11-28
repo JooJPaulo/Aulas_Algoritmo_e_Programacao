feac0a892c2c3f1143190872f57ea3b1b92f8855
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2023-11-28 00:12:06 +0000

    Lista final

--- lista.c
+++ lista.c
@@ -3,20 +3,134 @@
 typedef struct {
     int id;
     char nome[50];
+    int idade;
     struct Pessoa *prox;
 } Pessoa;
 
-Pessoa *cadastrar(Pessoa *lista) {
+Pessoa *criarPessoa() {
     Pessoa *p = (Pessoa *) malloc(sizeof(Pessoa));
     
     if (p == NULL) {
         printf("Erro ao alocar memoria\n");
         exit(1);
     }
-
     p->prox = NULL;
     return p;
+}
 
+Pessoa *cadastrar(Pessoa *lista) {
+    Pessoa *p = criarPessoa();
+
+    printf("Digite o nome: ");
+    getchar(); 
+    fgets(p->nome, sizeof(p->nome), stdin);
+
+    printf("Digite a idade: ");
+    scanf("%d", &p->idade);
+
+    if (lista == NULL) {
+        p->id = 1;
+        lista = p;
+    } else {
+        Pessoa *atual = lista;
+        while (atual->prox != NULL) {
+            atual = atual->prox;
+        }
+        p->id = atual->id + 1; 
+        atual->prox = p;
+    }
+    return lista;
+}
+
+
+Pessoa *listar(Pessoa *lista) {
+    if (lista == NULL) {
+        printf("Lista vazia\n");
+    } else {
+        Pessoa *atual = lista;
+        while(atual != NULL) {
+            printf("ID: %d\n", atual->id);
+            printf("Nome: %s", atual->nome);
+            printf("Idade: %d\n", atual->idade);
+            atual = atual->prox;
+        }
+    }
+    return lista;
+}
+
+Pessoa *buscar(Pessoa *lista) {
+    if (lista == NULL) {
+        printf("Lista vazia\n");
+    } else {
+        int id;
+        printf("Digite o ID: ");
+        scanf("%d", &id);
+
+        Pessoa *atual = lista;
+        while(atual != NULL) {
+            if (atual->id == id) {
+                printf("ID: %d\n", atual->id);
+                printf("Nome: %s", atual->nome);
+                printf("Idade: %d\n", atual->idade);
+                break;
+            }
+            atual = atual->prox;
+        }
+    }
+    return lista;
+}
+
+Pessoa *alterar(Pessoa *lista) {
+    if (lista == NULL) {
+        printf("Lista vazia\n");
+    } else {
+        int id;
+        printf("Digite o ID: ");
+        scanf("%d", &id);
+
+        Pessoa *atual = lista;
+        while (atual != NULL) {
+            if (atual->id == id) {
+                getchar();
+                printf("Digite o nome: ");
+                fgets(atual->nome, sizeof(atual->nome), stdin);
+
+                printf("Digite a idade: ");
+                scanf("%d", &atual->idade);
+                break;
+            }
+            atual = atual->prox;
+        }
+    }
+    return lista;
+}
+
+
+Pessoa *excluir(Pessoa *lista) {
+    if (lista == NULL) {
+        printf("Lista vazia\n");
+    } else {
+        int id;
+        printf("Digite o ID: ");
+        scanf("%d", &id);
+
+        Pessoa *atual = lista;
+        Pessoa *anterior = NULL;
+        while(atual != NULL) {
+            if (atual->id == id) {
+                if (anterior == NULL) {
+                    lista = atual->prox;
+                } else {
+                    anterior->prox = atual->prox;
+                }
+                free(atual);
+                break;
+            }
+            anterior = atual;
+            atual = atual->prox;
+        }
+    }
+    return lista;
 }
 
 main() {
@@ -34,28 +148,27 @@ main() {
         scanf("%d", &opcao);
         switch(opcao) {
             case 1:
-                printf("Cadastrar uma pessoa\n");
+                lista = cadastrar(lista);
                 break;
             case 2:
-                printf("Listar todos\n");
+                lista = listar(lista);
                 break;
             case 3:
-                printf("Buscar uma pessoa\n");
+                lista = buscar(lista);
                 break;
             case 4:
-                printf("Alterar uma pessoa\n");
+                lista = alterar(lista);
                 break;
             case 5:
-                printf("Excluir uma pessoa\n");
+                lista = excluir(lista);
                 break;
             case 0:
-                printf("Sair\n");
+                printf("Saindo...\n");
                 break;
             default:
                 printf("Opcao invalida\n");
         }
     } while(opcao != 0);
 
-    
-
+    return 0;
 }
